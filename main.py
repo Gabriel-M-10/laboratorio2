fad78888309cb08e8278e20ab9d2fc5e48becf11
Gabriel <gimendozam@estudiante.intellectus.edu.gt>
2023-06-15 09:23:29 -0600

    operaciones

---
+++ main.py
@@ -0,0 +1,16 @@
+import operaciones
+
+def menu():
+    print("Selecciona una opción:")
+    print("1. Suma")
+    print("2. Resta")
+    print("3. Multiplicación")
+    print("4. División")
+    print("5. Exponenciación")
+    print("6. Raíz cuadrada")
+    print("7. Suma de 5 números")
+    print("8. Residuo")
+    print("9. Quetzal a Dólar")
+    print("10. Dólar a Quetzal")
+    print("0. Salir")
+    
