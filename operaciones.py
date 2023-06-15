81c1d0bc9c481cf983ee6998153bccd6ec547ff9
Gabriel <gimendozam@estudiante.intellectus.edu.gt>
2023-06-15 08:55:42 -0600

    division

--- operaciones.py
+++ operaciones.py
@@ -8,3 +8,9 @@ def resta(a, b):
 
 def multiplicacion(a, b):
     return a * b
+
+def division(a, b):
+    if b != 0:
+        return a / b
+    else:
+        return "Error: No se puede dividir entre cero."
