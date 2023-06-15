238436dddd9847d2bcf5cc307ddeb82a16e40c2f
Gabriel <gimendozam@estudiante.intellectus.edu.gt>
2023-06-15 09:40:23 -0600

    realizaroperacion

--- main.py
+++ main.py
@@ -13,4 +13,62 @@ def menu():
     print("9. Quetzal a Dólar")
     print("10. Dólar a Quetzal")
     print("0. Salir")
+
+    def realizar_operacion(opcion):
+    if opcion == 1:
+        a = float(input("Ingrese el primer número: "))
+        b = float(input("Ingrese el segundo número: "))
+        resultado = operaciones.suma(a, b)
+        print("El resultado es:", resultado)
+    elif opcion == 2:
+        a = float(input("Ingrese el primer número: "))
+        b = float(input("Ingrese el segundo número: "))
+        resultado = operaciones.resta(a, b)
+        print("El resultado es:", resultado)
+    elif opcion == 3:
+        a = float(input("Ingrese el primer número: "))
+        b = float(input("Ingrese el segundo número: "))
+        resultado = operaciones.multiplicacion(a, b)
+        print("El resultado es:", resultado)
+    elif opcion == 4:
+        a = float(input("Ingrese el primer número: "))
+        b = float(input("Ingrese el segundo número: "))
+        resultado = operaciones.division(a, b)
+        print("El resultado es:", resultado)
+    elif opcion == 5:
+        a = float(input("Ingrese la base: "))
+        b = float(input("Ingrese el exponente: "))
+        resultado = operaciones.exponenciacion(a, b)
+        print("El resultado es:", resultado)
+    elif opcion == 6:
+        a = float(input("Ingrese el número: "))
+        resultado = operaciones.raiz_cuadrada(a)
+        print("El resultado es:", resultado)
+    elif opcion == 7:
+        a = float(input("Ingrese el primer número: "))
+        b = float(input("Ingrese el segundo número: "))
+        c = float(input("Ingrese el tercer número: "))
+        d = float(input("Ingrese el cuarto número: "))
+        e = float(input("Ingrese el quinto número: "))
+        resultado = operaciones.suma_5_numeros(a, b, c, d, e)
+        print("El resultado es:", resultado)
+    elif opcion == 8:
+        a = float(input("Ingrese el primer número: "))
+        b = float(input("Ingrese el segundo número: "))
+        resultado = operaciones.residuo(a, b)
+        print("El resultado es:", resultado)
+    elif opcion == 9:
+        quetzal = float(input("Ingrese la cantidad en Quetzales: "))
+        resultado = operaciones.quetzal_a_dolar(quetzal)
+        print("El resultado es:", resultado)
+    elif opcion == 10:
+        dolar = float(input("Ingrese la cantidad en Dólares: "))
+        resultado = operaciones.dolar_a_quetzal(dolar)
+        print("El resultado es:", resultado)
+    elif opcion == 0:
+        print("Saliendo del programa...")
+        return False
+    else:
+        print("Opción inválida. Por favor, ingrese un número válido.")
     
+    return True
